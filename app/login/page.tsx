45e85bcd3a2c338fd3d0fb00f172395b1d76dd63
Swastik <bairagiswastik@gmail.com>
2023-10-11 12:56:46 +0530

    deploy app

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import AuthButtonClient from "../auth-button-client";
+import GitHubButton from "./github-button";
+
+export const dynamic = 'force-dynamic'; 
+
+export default async function Login() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-1 flex justify-center items-center">
+      <GitHubButton />
+    </div>
+  );
+}
